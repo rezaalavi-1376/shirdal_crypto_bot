6e7eaaae8d50f2fd1d51c2edf47196ed7324d11d
rezaalavi-1376 <rezaalavi.sarina.1404@gmail.com>
2025-08-02 07:56:45 +0330

    Create bot.py
    افزودن فایل bot.py برای ربات

---
+++ bot.py
@@ -0,0 +1,31 @@
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, CallbackContext
+import requests
+
+TOKEN =8420874955:AAGnNlb-uFUGEAQiUdlhh95QmywWpQ_aoMA
+
+def get_price(symbol):
+    url = f'https://api.binance.com/api/v3/ticker/price?symbol={symbol}USDT'
+    res = requests.get(url)
+    data = res.json()
+    return f"{symbol.upper()}: ${data['price']}"
+
+def start(update: Update, context: CallbackContext):
+    update.message.reply_text('سلام! نماد رمزارز مورد نظرتو با / بنویس، مثل /BTC')
+
+def handle_symbol(update: Update, context: CallbackContext):
+    symbol = update.message.text[1:].upper()
+    try:
+        price = get_price(symbol)
+        update.message.reply_text(price)
+    except:
+        update.message.reply_text("نماد اشتباهه یا مشکلی در دریافت اطلاعات هست.")
+
+updater = Updater(TOKEN)
+dp = updater.dispatcher
+
+dp.add_handler(CommandHandler("start", start))
+dp.add_handler(CommandHandler(None, handle_symbol))
+
+updater.start_polling()
+updater.idle()
